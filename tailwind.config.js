56dff1674d3f76ff11baed38e9522bc450ee8b5a
Thiago Jansen <tjbeirao@gmail.com>
2024-04-17 18:41:41 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
